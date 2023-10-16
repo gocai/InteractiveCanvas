0d3958b82776401f56fd03c17d56dc90ec321140
Gordon Cai <gocai@ucsc.edu>
2023-10-16 16:42:50 -0700

    step 1 complete- canvas w border and shadow

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,20 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//step 1
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.querySelector("#canvas");
+const ctx = canvas.getContext("2d")!;
+const cWidth = canvas.width = 256;
+const cHeight = canvas.height = 256;
+const a = 0;
+const b = 0;
+ctx.fillStyle = "white";
+ctx.fillRect(a, b, cWidth, cHeight);
+ctx.strokeStyle = "black";
+ctx.strokeRect(a, b, canvas.width, canvas.height);
+
+
+
+
