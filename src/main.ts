77077e4ad886f22e6276e079a5810282ef819587
Gordon Cai <gocai@ucsc.edu>
2023-10-22 16:47:37 -0700

    step 3 finished

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,12 @@ canvas.querySelector("#canvas");
 const ctx = canvas.getContext("2d")!;
 canvas.height = 256;
 canvas.width = 256;
+
 const a = 0;
 const b = 0;
+const cursor = { active: false, x: 0, y: 0 };
+const one = 1;
+const zero = 0;
 
 function drawCanvas() {
     ctx.fillStyle = "white";
@@ -28,41 +32,110 @@ function drawCanvas() {
     ctx.strokeRect(a, b, canvas.width, canvas.height);
 }
 drawCanvas();
-//step 2
+
+interface Point{
+  x: number;
+  y: number;
+}
+
+const points: Point[][] = [];
+let currentLine: Point[] = [];
+const redoLines: Point[][] = [];
 
 
-const cursor = { active: false, x: 0, y: 0 };
+const bus = new EventTarget();
+
+function notify(name:string) {
+  bus.dispatchEvent(new Event(name));
+}
+bus.addEventListener("drawing-changed", redraw);
+
+
 
 canvas.addEventListener("mousedown", (e) => {
-    cursor.active = true;
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+  currentLine = [];
+  points.push(currentLine);
+  redoLines.splice(zero, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+  notify("drawing-changed");
+  
+  
   });
+  
+
   
   canvas.addEventListener("mousemove", (event) => {
     if (cursor.active) {
-      ctx.beginPath();
-      ctx.moveTo(cursor.x, cursor.y);
-      ctx.lineTo(event.offsetX, event.offsetY);
-      ctx.stroke();
+      
       cursor.x = event.offsetX;
       cursor.y = event.offsetY;
+      points.push(currentLine);
+      currentLine.push({ x: cursor.x, y: cursor.y });
+      notify("drawing-changed");
     }
   });
   
-  window.addEventListener("mouseup", () => {
+  
+  canvas.addEventListener("mouseup", () => {
     cursor.active = false;
+    currentLine = [];
+    notify("drawing-changed");
   });
   
+  function redraw() {
+    ctx.clearRect(zero, zero, canvas.width, canvas.height);
+    drawCanvas();
+    for (const line of points) {
+      if (line.length > one) {
+        ctx.beginPath();
+        const { x, y } = line[zero];
+        ctx.moveTo(x, y);
+        for (const { x, y } of line) {
+          ctx.lineTo(x, y);
+        }
+        ctx.stroke();
+        
+      }
+    }
+    console.log("redraw was called");
+  }
+
   const clear = document.createElement("button");
   clear.innerHTML = "clear";
   app.append(clear);
   
-  clear.addEventListener("click", () => {
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawCanvas();
-  });
+clear.addEventListener("click", (clearCanvas));
+function clearCanvas() {
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  points.splice(0, points.length);
+  console.log(points);
+  notify("drawing-changed");
+  }
 
+  const undoButton = document.createElement("button");
+  undoButton.innerHTML = "undo";
+  app.append(undoButton);
 
+undoButton.addEventListener("click", (undoCanvas));
+function undoCanvas() {
+  
+  if (points.length > zero) {
+    redoLines.push(points.pop()!);
+    notify("drawing-changed");
+  }
+}
 
+  const redoButton = document.createElement("button");
+  redoButton.innerHTML = "redo";
+  app.append(redoButton);
+
+redoButton.addEventListener("click", (redoCanvas));
+function redoCanvas() {
+  if (redoLines.length > zero) {
+    points.push(redoLines.pop()!);
+    notify("drawing-changed");
+  }
+}
